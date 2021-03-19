b9df7c117fe71b0616caecb14a3fd2ba1a9aac8e
RealPolitiX <xrpatrick@gmail.com>
2021-03-19 01:50:11 -0500

    Added bash scripts for reconstruction benchmarks

---
+++ examples/scripts/WSe2_HSL_recon_benchmark.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## Reconstruct 2 valence bands of WSe2
+NITER=100 # Number of iteractions in reconstruction optimization
+KSCALE=1
+PYTHONPATH="/cygdrive/c/ProgramData/Anaconda3/python"
+CODEPATH="./WSe2_HSL_recon.py"
+FPATH="WSe2_HSL_recon.txt"
+> $FPATH
+
+BIDS="1 2 3 4 5 6 7 8 9 10 11 12 13 14"
+for BID in $BIDS
+echo "Tuning initial conditions for reconstructing band #$BID ..."
+do
+    echo "current band = $BID, energy shift = $SHFTS" >> $FPATH
+    $PYTHONPATH $CODEPATH -bid=$BID -ksc=$KSCALE -pm='benchmark' -niter=$NITER >> $FPATH
+    echo "" >> $FPATH
+done
